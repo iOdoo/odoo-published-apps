569d87c32e9b58cb76a0f1c9ff2cd73a25cf7997
Kamrul Hasan <kamrul.devcs@gmail.com>
2021-11-01 15:23:42 +0600

    add banner

--- ibos_pos_discount_control/__manifest__.py
+++ ibos_pos_discount_control/__manifest__.py
@@ -1,4 +1,26 @@
 # -*- coding: utf-8 -*-
+###################################################################################
+#
+#    iBOS Limited.
+#    Copyright (C) 2021-TODAY iBOS Limited (<https://www.ibos.io>).
+#    Author: Kamrul Hasan (<https://www.ibos.io>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+# -*- coding: utf-8 -*-
 {
     'name': "Discount Control on POS",
 
@@ -28,6 +50,7 @@
     'qweb': [],
     # only loaded in demonstration mode
     'demo': [],
+    'images': ['static/description/banner.png'],
     'installable': True,
     'auto_install': False,
     'application': True,
